40c9bc05fd6da6bb78945ca9e8c2658eaa9b8e27
Geoff Lane <geoff@zorched.net>
2019-05-13 10:23:37 -0400

    Script to setup terminal colors

---
+++ gnome-terminal-colors.sh
@@ -0,0 +1,17 @@
+# This should be "Default"
+profile_id=b1dcc9dd-5262-4d8d-a863-c897e6d979b9
+
+dconf write /org/gnome/terminal/legacy/profiles:/:$profile_id/bold-color-same-as-fg "true"
+dconf write /org/gnome/terminal/legacy/profiles:/:$profile_id/use-theme-colors "false"
+dconf write /org/gnome/terminal/legacy/profiles:/:$profile_id/background-transparency-percent "0"
+dconf write /org/gnome/terminal/legacy/profiles:/:$profile_id/use-transparent-background "true"
+dconf write /org/gnome/terminal/legacy/profiles:/:$profile_id/palette "['#282a2e', '#a54242', '#8c9440', '#de935f', '#5f819d', '#85678f', '#5e8d87', '#707880', '#373b41', '#cc6666', '#b5bd68', '#f0c674', '#81a2be', '#b294bb', '#8abeb7', '#c5c8c6']"
+dconf write /org/gnome/terminal/legacy/profiles:/:$profile_id/background-color "'#151517161818'"
+dconf write /org/gnome/terminal/legacy/profiles:/:$profile_id/visible-name "'Default'"
+dconf write /org/gnome/terminal/legacy/profiles:/:$profile_id/bold-color "'#000000'"
+dconf write /org/gnome/terminal/legacy/profiles:/:$profile_id/foreground-color "'#5c8c6'"
+dconf write /org/gnome/terminal/legacy/profiles:/:$profile_id/background-color "'#1d1f21'"
+dconf write /org/gnome/terminal/legacy/profiles:/:$profile_id/cursor-color "'#c5c8c6'"
+dconf write /org/gnome/terminal/legacy/profiles:/:$profile_id/use-theme-transparency "false"
+dconf write /org/gnome/terminal/legacy/profiles:/:$profile_id/font "'Hack 11'"
+
