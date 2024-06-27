c3fadff7d13ee5d95027d9407240a18d580d4fba
Geoff Lane <geoff@zorched.net>
2024-06-27 19:24:24 +0000

    Snippet support

---
+++ nvim/lua/plugins/coding.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+  },
+
+  { "garymjr/nvim-snippets", lazy = true },
+  { "rafamadriz/friendly-snippets", lazy = true },
+}
