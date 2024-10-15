b53fe22847978c5056658974c1a3c52b7b96d623
Geoff Lane <geoff@zorched.net>
2024-10-15 12:34:51 +0000

    Preload nordic

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -22,6 +22,11 @@ return {
   {
     "AlexvZyl/nordic.nvim",
     name = "nordic",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("nordic").load()
+    end,
   },
 
   {
