db3cf9328d3359be37e4762e19cc857620c3e5db
Geoff Lane <geoff@zorched.net>
2024-11-12 20:16:21 -0500

    Does this get the right arch?

--- setup.sh
+++ setup.sh
@@ -140,8 +140,14 @@ pip install neovim
 gem install neovim
 
 if [ ! -x "$(command -v lazygit)" ]; then
+  if [ $(arch) == "aarch64"]; then
+    ARCH="arm64"
+  else
+    ARCH=$(arch)
+  fi
+
   LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-  curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+  curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_${ARCH}.tar.gz"
   tar xf lazygit.tar.gz lazygit
   sudo install lazygit /usr/local/bin
 fi
