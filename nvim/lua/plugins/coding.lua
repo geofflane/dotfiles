cc1753069217e9967c5eb4df6cf62d83b5ffea60
Geoff Lane <geoff@zorched.net>
2024-12-13 16:12:30 +0000

    Lazyvim no longer uses these?

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -1,9 +1,9 @@
 return {
-  {
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-  },
+  -- {
+  --   "hrsh7th/cmp-nvim-lsp",
+  --   "hrsh7th/cmp-buffer",
+  --   "hrsh7th/cmp-path",
+  -- },
 
   { "garymjr/nvim-snippets", lazy = true },
   { "rafamadriz/friendly-snippets", lazy = true },
