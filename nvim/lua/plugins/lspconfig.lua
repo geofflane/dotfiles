012d420fc244ce966c006f11ada33ab9ca3fc824
Geoff Lane <geoff@zorched.net>
2025-08-29 14:45:26 +0000

    Enable expert

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -2,10 +2,16 @@ return {
 
   {
     "neovim/nvim-lspconfig",
+    init = function()
+      vim.lsp.enable("expert")
+    end,
     opts = {
       servers = {
         bashls = {},
         somesass_ls = {},
+        elixirls = {
+          enabled = false,
+        },
       },
     },
   },
